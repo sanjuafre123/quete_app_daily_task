aa9a6025e50d769c4ee0d01d403a0e5ea2512bd5
Sanju afre <148860124+sanjuafre123@users.noreply.github.com>
2024-06-03 20:30:57 +0530

    Add files via upload

---
+++ lib/Random_Quote/Model.dart
@@ -0,0 +1,21 @@
+
+import 'package:quote_app_daily_task/Random_Quote/quoteList.dart';
+
+class QuoteModel {
+  String? quote;
+  String? author;
+
+  QuoteModel({this.quote, this.author});
+
+  List<QuoteModel> quoteModelList = [];
+
+  factory QuoteModel.formQuote(Map data) {
+    return QuoteModel(author: data['author'], quote: data['quote']);
+  }
+
+  QuoteModel.toList(List l1) {
+    for (int i = 0; i < l1.length; i++) {
+      quoteModelList.add(QuoteModel.formQuote(l1[i]));
+    }
+  }
+}
---
+++ lib/Random_Quote/quote.dart
@@ -0,0 +1,129 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:quote_app_daily_task/Random_Quote/Model.dart';
+import 'package:quote_app_daily_task/Random_Quote/quoteList.dart';
+
+QuoteModel? quoteModel;
+
+class QuoteScreen extends StatefulWidget {
+  const QuoteScreen({super.key});
+
+  @override
+  State<QuoteScreen> createState() => _QuoteScreenState();
+}
+
+class _QuoteScreenState extends State<QuoteScreen> {
+  void initState() {
+    quoteModel = QuoteModel.toList(quotesList);
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: (
+        Icon(Icons.format_quote_outlined,size: 30,)
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(Icons.format_quote_outlined,size: 30,),
+          ),
+        ],
+        centerTitle: true,
+        elevation: 2,
+        shadowColor: Colors.grey,
+        title: const Text(
+          'Quotes',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ...List.generate(
+              quoteModel!.quoteModelList.length,
+              (index) => Card(
+                color: cardColors[index % cardColors.length],
+                child: ListTile(
+                  title: Text(
+                    quoteModel!.quoteModelList[index].quote!,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w400),
+                  ),
+                  subtitle: Text(
+                    quoteModel!.quoteModelList[index].quote!,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Random random = Random();
+          int x = random.nextInt(quoteModel!.quoteModelList.length);
+
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text(
+                quoteModel!.quoteModelList[x].author!,
+                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22),
+              ),
+              content: Text(
+                quoteModel!.quoteModelList[x].quote!,
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      Navigator.of(context).pop();
+                    });
+                  },
+                  child: Text(
+                    'back',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      Navigator.of(context).pop();
+                    });
+                  },
+                  child: Text(
+                    'save',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        backgroundColor: Colors.black,
+        child: Icon(
+          Icons.notification_add,
+          color: Colors.white,
+          size: 34,
+        ),
+      ),
+    );
+  }
+}
